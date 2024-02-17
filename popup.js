c609393f9caf861581d44ac840f1134ea9fb6129
Mohammad Reza Tayyebi <tayyebi_mohammadreza@hotmail.com>
2024-02-17 12:48:40 +0330

    کپی شد.

--- popup.js
+++ popup.js
@@ -34,13 +34,26 @@ document.addEventListener("DOMContentLoaded", function () {
         const trackButton = document.createElement("button");
         trackButton.textContent = "رهگیری";
         trackButton.addEventListener("click", function () {
-          const value = pairs[key]; // Retrieve the value associated with the key
-          // Open the tracking website with the value as a query parameter
-          const trackingUrl = `https://tracking.post.ir/`;
-          window.open(trackingUrl, "_blank");
+          const value = pairs[key];
+
+          copyTextToClipboard(value);
+          showToast();
+
+          // Check if the current tab has the same URL as the tracking website
+          chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            const currentTabUrl = tabs[0].url;
+            const trackingUrl = "https://tracking.post.ir";
+
+            if (currentTabUrl == trackingUrl) {
+
+              window.open(trackingUrl, "_blank");
+
+            }
+
+          });
 
           // Optionally, insert the value into the text box (if the tracking website has one)
-          const trackingCodeInput = getElementByXpath("//html[1]/body[1]/div[1]");
+          const trackingCodeInput = getElementByXpath("/html/body/form/div[6]/div/div/div[1]/div[1]/div/input");
           if (trackingCodeInput) {
             trackingCodeInput.value = value;
             trackingCodeInput.form.submit();
@@ -69,6 +82,48 @@ document.addEventListener("DOMContentLoaded", function () {
       pairs[key] = value;
       chrome.storage.local.set({ pairs });
       displayPairs();
+    });
+  }
+
+
+  function showToast() {
+    const toast = document.getElementById("toast");
+    toast.classList.add("show");
+
+    setTimeout(() => {
+      toast.classList.remove("show");
+    }, 1000); // Hide after 3 seconds
+  }
+
+  // https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+  function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+      console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+  }
+  function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(text);
+      return;
+    }
+    navigator.clipboard.writeText(text).then(function () {
+      console.log('Async: Copying to clipboard was successful!');
+    }, function (err) {
+      console.error('Async: Could not copy text: ', err);
     });
   }
 
